8e1cc5abe6863ab53d358be0ad97c11f033f4d7a
Tony Wu <tonycwu888@gmail.com>
2016-11-08 01:03:21 -0800

    added details to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#SPECIES ------------------------
+#Dog
+
+#CHARACTERISTICS ----------------
+#Tail: varies
+#Friendly: yes
+#Fur type: varies
+#Name: varies
+#Color: varies
+
+#BEHAVIOR -----------------------
+#Jump
+#Run
+#Bark
+#Lick
