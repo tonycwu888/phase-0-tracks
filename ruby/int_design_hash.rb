25fb86b74fd14284cbce60f93215a25c7b5f3c00
Tony Wu <tonycwu888@gmail.com>
2016-11-12 15:28:31 -0800

    added class to shout.rb and changing standalone module to mixin module

---
+++ ruby/int_design_hash.rb
@@ -0,0 +1,20 @@
+designing_types = {
+    name: "tony",
+    address: "333 beauti St",
+    email: "beautiful@gmail.com",
+    phone: "415-415-4455",
+    Fave_color: "navy blue",
+
+}
+
+p designing_types
+p designing_types.keys
+p designing_types.values
+
+p designing_types[:name]
+designing_types[:name2] = "jonny"
+p designing_types
+
+p designing_types[:name] + designing_types[:address]
+p designing_types[:name].upcase
+p designing_types
