7b78c2da3126723eaf2c6a7deb407082a85df3c1
Tony Wu <tonycwu888@gmail.com>
2016-11-15 10:51:55 -0800

    created function to return new object of horseBarn

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,16 @@ var horseName = ['Jeff', 'Steven', 'Tony', 'Jonah'];
 
 horseColor.push('black');
 horseName.push('John');
+
+
+function makeObject(arryKey, arryValue) {
+  var newObject = {};
+    for (idx=0; idx < arryKey.length; idx++)  {
+      newObject[arryKey[idx]] = arryValue[idx];
+    }
+  return newObject;
+}
+
+var horseBarn = makeObject(horseName, horseColor);
+
+console.log(horseBarn);
