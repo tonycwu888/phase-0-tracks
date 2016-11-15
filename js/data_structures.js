e7cdae9179576b75fd9a5d07c85076e9643025d5
Tony Wu <tonycwu888@gmail.com>
2016-11-15 12:00:33 -0800

    create Car constructor function initialize instances of car and tested contructor function with driver code

--- js/data_structures.js
+++ js/data_structures.js
@@ -16,3 +16,33 @@ function makeObject(arryKey, arryValue) {
 var horseBarn = makeObject(horseName, horseColor);
 
 console.log(horseBarn);
+
+function Car(color, make, model) {
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.passengers = [];
+
+    this.brake = function() {
+      console.log("STOP>>>>>");
+    }
+    this.accelerate = function()  {
+      console.log("Zooom...");
+    }
+    this.getInCar = function(name)  {
+      this.passengers.push(name);
+      console.log(name + " is now in the " + this.make + " " + this.model + "." );
+    }
+}
+
+var fancyCar = new Car("black", "lincoln", "towncar");
+var lyftCar = new Car("gray", "toyota", "prius");
+fancyCar.getInCar("Leo");
+fancyCar.accelerate();
+fancyCar.brake();
+console.log(fancyCar.passengers);
+lyftCar.getInCar("drunk Marina bro");
+lyftCar.getInCar("drunk Marina girl");
+lyftCar.accelerate();
+lyftCar.brake();
+console.log(lyftCar.passengers);
