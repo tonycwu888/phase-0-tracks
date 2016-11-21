84ca0214e33dbe8a9e4719e681d24c50d7efb82d
Tony Wu <tonycwu888@gmail.com>
2016-11-21 04:58:08 -0800

    add randomTestData function, worked in repl but not in node, unfinished

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,4 @@
+//***phase 0 ******
 //function accept array as argument
 //count how many strings are in the array
 //count the letters in each string, locate the longest string
@@ -10,12 +11,9 @@
 function longestPhrase(input)  {
     var temp = [];
     //console.log(input.length);
-    for (i = 0; i < input.length; i++) {
+    for (var i = 0; i < input.length; i++) {
       console.log(input[i].length);  //length of string
-      //console.log (input[i]); //string
-      //if ((input[i].length) < (input[i+1].length)) {
 
-      //}
       console.log (temp.push(input[i].length)); //pushing string into new array
   // i was able to find the lengths of each string, but fail to pull the index of the longest string and print it
       var lengthTemp = temp.sort();
@@ -25,6 +23,7 @@ function longestPhrase(input)  {
 //console.log(input[index of longest string])
   }
 
+//  **** phase 1 ******
 //finding a key value match
 //create a set of key and values to pass into function
 //function will accept 2 sets of key and values
@@ -36,23 +35,47 @@ function longestPhrase(input)  {
 
 
 function keyValueMatch(input1, input2) {
- var keyValues = [input1, input2];
- console.log(keyValues[0] == keyValues[1]);
- console.log(keyValues[0]);
+  var keyValues = [input1, input2];
+  console.log(keyValues[0] == keyValues[1]);
+  console.log(keyValues[0]);
   console.log(keyValues[1]);
 
 //i'm not understanding what is going on system is
 //evaluating below if statement being true and when trying to print the key and value, it shows undefined
 // trying to compare the vaules of the keys to see if any of them are equal so I can return
 //true, but can't seem to understanding why below if statement is evaluating "true"
- if (input1.key == input2.value){
-   console.log('hey');
-   console.log(input2.value);
-   console.log(input1.key);
- }
-}
+  if (input1.key == input2.value){
+    console.log('hey');
+    console.log(input2.value);
+    console.log(input1.key);
+  }
 }
 
+//***phase 2 ******
+//generate random test data
+//create function to accept int
+//function will generate 3 random strings in an array (each string with length of 1-10 characters)
+//input == array.length
+//keep creating random strings until array.length is reached --
+//if array.length is reach, return array of strings
+//example input:  3
+//example of possible output: ["ksdjdj", "ee", "utmdklllp"]
+
+function randomTestData(numStr) {
+  var text = "";
+  var ranArry = [];
+  var possible = "abcdefghijklmnopqrstuvwxyz";
+  for (var i = 0; i < numStr; i++) {
+    if (ranArry.length < numStr) {
+      text += possible.charAt(Math.floor(Math.random() * 10));
+      ranArry.push(text);
+    }
+
+  }
+  return ranArry;
+
+}
+randomTestData(6);
 
 longestPhrase(["tommy", "ray ray jones", "killer jonny", "mad max maxwell"]);
 
