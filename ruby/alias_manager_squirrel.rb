3d96598f9460ab946e4d42db2f256f4ecd7a326f
Tony Wu <tonycwu888@gmail.com>
2016-11-26 21:43:48 -0800

    create alias_manager_squirrel.rb

---
+++ ruby/alias_manager_squirrel.rb
@@ -0,0 +1,19 @@
+#swap first name and last name
+#  --split string into words.
+#  --swap places
+#swap letters of first name and last name
+#  --if letter is a,e,i,o,u.. move to the next vowel
+#  --if letter is consonant, move to the next consanant
+
+#use string and split into 2 words
+#store it to name reverse / switch spots of first
+#and last name on index
+#create vowel string aeiou = 4
+#create consonatnts string bcdfghjklmnpqrstvwxyz =20
+#if vowel, move to next vowel
+#if consonants, move to next consonatnts
+
+
+
+#example input: jonny wisdom
+#example output:  xotfun kuppz
