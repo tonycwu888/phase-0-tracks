1450c7dae3a5177259209893cc93fc421a12c8fd
Tony Wu <tonycwu888@gmail.com>
2016-11-28 01:22:30 -0800

    add sort method for problem_solving_methods_squirrel.rb

--- ruby/problem_solving_methods_squirrel.rb
+++ ruby/problem_solving_methods_squirrel.rb
@@ -40,3 +40,39 @@
   	fib(10)
   	fib(59)
   	fib(100)
+
+    # Release 2: Sort an Array
+    	#Define method
+    		#find length of array
+    		#loop according to length (x-1) times for length x
+    		#compare content of numbers in array
+    		#end loop when sorted
+    		#display sorted array
+  puts "======================="
+    def sort(array)
+    	x = array.length
+    	loop do
+    		changed = false
+    		(x-1).times do |y|
+    			if array[y] > array [y+1]
+    				array[y], array[y+1] = array[y+1], array[y]
+    				changed = true
+    			end
+    		end
+
+    		break if not changed
+    	end
+    	array
+    end
+
+    	first = [3, 9, 5, 10, 2, 85, 1 ]
+    	second = [ 31412, 213, 45678, 2, 1]
+
+    p "Before sort"
+    p first
+    p "After sort"
+    p sort(first)
+    p "Before sort"
+    p second
+    p "After sort"
+    p sort(second)
