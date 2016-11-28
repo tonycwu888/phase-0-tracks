c5cc4502badf373d9e5093035bdec3954dee568d
Tony Wu <tonycwu888@gmail.com>
2016-11-27 20:55:32 -0800

    add each and map! in iteration_squirrel.rb

--- ruby/iteration_squirrel.rb
+++ ruby/iteration_squirrel.rb
@@ -5,3 +5,39 @@ def simple_print
 end
 
 simple_print { |x| puts x * 3 }
+puts "================================"
+
+dead_celebrities = ['Elvis Presley',
+    'Bruce Lee',
+    'Whitney Houston',
+    'James Brown',
+    'Jim Morrison',
+    'James Dean',
+    'Marvin Gaye'
+]
+
+people_currentcy = {
+    one: 'Washington',
+    two: 'Jefferson',
+    five: 'Lincoln',
+    ten: 'Hamilton',
+    twenty: 'Jackson',
+    fifty: 'Grant',
+    hundred: 'Franklin',
+    thousand: 'Cleveland'
+}
+
+dead_celebrities.each do |name|
+  puts "#{name} is dead."
+end
+
+puts "================================"
+puts "before map! iteration #{dead_celebrities}"
+puts "================================"
+dead_celebrities.map! do |name|
+  name.upcase!
+  puts "#{name} is now in all caps!"
+end
+puts "================================"
+puts "after map! iteration #{dead_celebrities}"
+puts "================================"
