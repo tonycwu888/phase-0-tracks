6c98f4ee91858ce755fabe3f41e64d0d5cce324d
Tony Wu <tonycwu888@gmail.com>
2016-11-27 23:08:31 -0800

    iterate through arrays and hash on iteration_squirrel.rb

--- ruby/iteration_squirrel.rb
+++ ruby/iteration_squirrel.rb
@@ -16,7 +16,7 @@ dead_celebrities = ['Elvis Presley',
     'Marvin Gaye'
 ]
 
-people_currentcy = {
+people_currency = {
     one: 'Washington',
     two: 'Jefferson',
     five: 'Lincoln',
@@ -34,10 +34,36 @@ end
 puts "================================"
 puts "before map! iteration #{dead_celebrities}"
 puts "================================"
+
 dead_celebrities.map! do |name|
-  name.upcase!
-  puts "#{name} is now in all caps!"
+  name.upcase
+  #puts "#{name} is now in all caps!"
 end
 puts "================================"
 puts "after map! iteration #{dead_celebrities}"
 puts "================================"
+
+#release 2.1
+#array
+p dead_celebrities.delete_if {|name| name == "BRUCE LEE"}
+#hash
+p people_currency.delete_if { |bill, name| name == "Grant"}
+
+#release 2.2
+#array
+p dead_celebrities.keep_if {|name| name.length <= 12}
+#hash
+p people_currency.keep_if {|bill, name| name.length <= 12}
+
+#release 2.3
+#array
+p dead_celebrities.reject {|name| name == 'JAMES BROWN'}
+#hash
+p people_currency.reject {|bill, name| name == 'Lincoln'}
+
+#release 2.4
+#array
+kids = ["Tommy", "Bobby", "Joey", "Bobo", "Samantha", "Bob"]
+p kids
+p kids[2]
+p kids.take_while { |kid| kid != kids[2] }
