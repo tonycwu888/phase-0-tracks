c6cc120391a48849204880b96c795dac74c27b80
Tony Wu <tonycwu888@gmail.com>
2016-11-28 01:15:35 -0800

    add Fibonacci numbers algorithm to problem_solving_methods_squirrel.rb

--- ruby/problem_solving_methods_squirrel.rb
+++ ruby/problem_solving_methods_squirrel.rb
@@ -17,7 +17,26 @@
 		end
 	end
 
-	search_num(numbers, 7)
-	search_num(numbers, 14)
+	search_num(numbers, 10)
+	search_num(numbers, 44)
 	search_num(numbers, 83)
-	search_num(numbers, 6)
+	search_num(numbers, 88)
+  puts "======================="
+
+# Release 1: Calculate Fibonacci Numbers
+
+  	def fib (x)
+  		init = [0,1]
+  		count = 1
+  		i = x - 1
+  		until count == i
+  			init << init[count - 1] + init[count]
+  			count += 1
+  		end
+  		p init
+  	end
+
+  	fib(6)
+  	fib(10)
+  	fib(59)
+  	fib(100)
