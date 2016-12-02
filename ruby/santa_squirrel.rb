62cfbdded0ab96d660d14ea52d5e72a546786c26
Tony Wu <tonycwu888@gmail.com>
2016-12-02 09:54:03 -0800

    added instance variables to santa_squirrel.rb

--- ruby/santa_squirrel.rb
+++ ruby/santa_squirrel.rb
@@ -1,6 +1,10 @@
 class Santa
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance...."
+    @gender = gender
+    @ethnicity = ethnicity
+    @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
   def speak
@@ -16,3 +20,12 @@ end
 santa = Santa.new
 santa.speak
 santa.eat_milk_and_cookies("oakmeal")
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
