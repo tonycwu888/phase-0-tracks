d7cb6b544ef14a8ee902e258e3f3c40d5e59b6e6
Tony Wu <tonycwu888@gmail.com>
2016-12-02 20:11:15 -0800

    intialized santas with gender, ethnicity, and weight

--- ruby/santa_squirrel.rb
+++ ruby/santa_squirrel.rb
@@ -22,8 +22,8 @@ end
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-weight = [200, 198, 250, 205, 220, 210, 300]
+weights = [200, 198, 250, 205, 220, 210, 300]
 example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i], weight[i])
+  santas << Santa.new(example_genders[i], example_ethnicities[i], weights[i])
   puts santas
 end
