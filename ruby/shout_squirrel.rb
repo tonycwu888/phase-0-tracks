0f52db89153ca26d68ecd1c591da6965177a448e
Tony Wu <tonycwu888@gmail.com>
2016-12-04 15:14:32 -0800

    convert shout_squirrel.rb standalone module to mixin module

--- ruby/shout_squirrel.rb
+++ ruby/shout_squirrel.rb
@@ -1,12 +1,26 @@
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words + "!!" + "Happy, happy, joy-joy!"
   end
 end
+#
+# p Shout.yell_angrily("AWWWWWWWWWW, you Stu....id ")
+# p Shout.yell_happily("What a Wonderfuly day ")
 
-p Shout.yell_angrily("AWWWWWWWWWW, you Stu....id ")
-p Shout.yell_happily("What a Wonderfuly day ")
+class Wife
+  include Shout
+end
+
+class Son
+  include Shout
+end
+
+wife = Wife.new
+p wife.yell_angrily("Do your homework")
+
+son = Son.new
+p son.yell_happily("Yes mother dear, right away")
