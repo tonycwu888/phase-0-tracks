c4cb78fff39c19a1dd8891a2370a74ad674c5b2e
Tony Wu <tonycwu888@gmail.com>
2016-12-05 02:47:44 -0800

    refactored original wordguessing game

--- ruby/game_folder/solo_game_6_6.rb
+++ ruby/game_folder/solo_game_6_6.rb
@@ -25,106 +25,91 @@
 
 
 class WordGuessingGame
-  attr_accessor :chances, :secret_word, :letter_guessed, :each_let_guessed
-#initialize instance variables
-    def initialize(secret_word)
-      @chances = 0
-      @secret_word = secret_word
-      @let_arry_guessed = []
-      @each_let_guessed
+  attr_reader :chances
+
+  def initialize(secret_word)
+    @chances = 0
+    @secret_word = secret_word
+    @let_arry_guessed = []
+    @each_let_guessed
+  end
+
+  def initial_entry(input)
+    puts "_ " * input.length
+  end
+
+  def chances
+    @chances += 1
+  end
+
+  def guess_word(word)
+      word = word.to_s
+    if word == @secret_word
+      puts "You are so smart and amazing, the secret word is #{@secret_word}!"
+    else
+      puts "Better luck next time bozo!  You are awful at this game!"
     end
-    #prints how many letters in the secret word
-      def initial_entry(input)
-        puts "_ " * input.length
+  end
+
+  def guess_letter(letter)
+    return if @chances > @secret_word.length
+    temp_pass = @secret_word.split(//)
+    temp_pass.each do |pass|
+      if letter == pass
+        @let_arry_guessed << letter
+        let_index = @let_arry_guessed.index(pass)
+        @each_let_guessed = @let_arry_guessed[let_index]
+      else
+        @let_arry_guessed << letter
       end
-    #adds chances to counter
-        def chances
-          @chances += 1
-        end
-        #checks for users answer if they would like to enter the secret_word
-          def guess_word(word)
-            word = word.to_s
-            if word == @secret_word
-              puts "You are so smart and amazing, the secret word is #{@secret_word}!"
-            else
-              puts "Better luck next time sucker!  You are awful at this game!"
-            end
-          end
-          #check for user enter letter and store in array
-            def guess_letter(letter)
-                return if @chances > @secret_word.length * 2
-                temp_pass = @secret_word.split(//)
-                temp_pass.each do |pass|
-                  if letter == pass
-                    @let_arry_guessed << letter
-                    let_index = @let_arry_guessed.index(pass)
-                    @each_let_guessed = @let_arry_guessed[let_index]
-                  else
-                    @let_arry_guessed << letter
-                  end
-                  @let_arry_guessed.uniq!
-                end
-            end
-    #prints the user's result, how many times they entered and total amount of entries they have
-              def print_guessed_letters
-                #if their total chances have reached, exit
-                  return if @chances > @secret_word.length
-                  print_array = []
-                  count = 0
-                  while count < @secret_word.length
-                        if @secret_word[count] == @each_let_guessed
-                          print_array << @each_let_guessed
-                        elsif print_array[count] != "_ "
-                          print_array << "_ "
-                        end
-                        count += 1
-                  end
+        @let_arry_guessed.uniq!
+    end
+  end
 
-              puts "#{print_array.join}"
-              puts "You have guessed #{@chances} times"
-              puts "You have a total of #{@secret_word.length} chances."
-
-              end
+  def print_guessed_letters
+    return if @chances > @secret_word.length
+    print_array = []
+    count = 0
+    while count < @secret_word.length
+      if @secret_word[count] == @each_let_guessed
+        print_array << @each_let_guessed
+      elsif print_array[count] != "_ "
+        print_array << "_ "
+      end
+        count += 1
+    end
+    puts "#{print_array.join}"
+    puts "You have guessed #{@chances} times"
+    puts "You have a total of #{@secret_word.length} chances."
+  end
 end
 
-
 #user interface
-
 puts "Welcome to the Word Guessing Game."
 puts "Please enter a word for your friends to guess"
 secret_word = gets.chomp.to_s
-#initialize new class
-game = WordGuessingGame.new(secret_word)
-#clears the screen so user number 2 can try to guess what user one entered
 Gem.win_platform? ? (system "cls") : (system "clear")
 
+game = WordGuessingGame.new(secret_word)
 
-#displays how many letters are in the secret word
 game.initial_entry(secret_word)
 
+puts "Secret word is hidden and it has #{secret_word.length} letters."
+puts "You will only have #{secret_word.length} chances to guess the word."
+puts "Please enter 1 letter at time to try and figure out the word."
+puts "If you think you know the word, type the whole word in to win."
+puts "If you guess wrong, (GAME OVER)!!"
 
-
-puts "Secret word is hidden and it has this many letters:"
-puts "Please enter 1 letter at time to try and figure out the word.  If you think you know the word, type the whole word in to win.  If you guess wrong, (GAME OVER)!!"
-#loop to have user keep entering letter to figure out what the secret word is
-  count = 1
+count = 1
   until count > secret_word.length
     letter = gets.chomp.to_s
-    #increments his chance number after entering each letter
     game.chances
-
-
-    #when user is ready to enter the answer
     if letter.length > 1
-
       game.guess_word(letter)
       count = secret_word.length + 1
     else
-      #pass to guess_letter method to process entry
       game.guess_letter(letter)
-      #print out how many time user have tried and how many chances total
       game.print_guessed_letters
     end
     count += 1
-
   end
