31e443465d1e082663eda1b8119b630c361a3421
Tony Wu <tonycwu888@gmail.com>
2016-12-11 23:29:28 -0800

    add keyValueMatch function and test code

--- js/algos_squirrel.js
+++ js/algos_squirrel.js
@@ -20,5 +20,40 @@ function longestPhrase(phrase)  {
   return longestString;
 }
 
+/* phase 1 (find a key-value match)
+write a function that takes to object as arguement
+first, loop through key of object and see if there's any match
+  --if no match, loop over properties and see if match, if non found
+  --return false
+  --if any match is found, return true
+example input {name: "Steven", age: 54} and {name: "Tamir", age: 54}
+example result:  true
+*/
+
+function keyValueMatch(obj1, obj2)  {
+  var match = false;
+  var objectKey1 = Object.keys(obj1);
+  var objectKey2 = Object.keys(obj2);
+  for (i = 0; i < objectKey1.length; i ++)  {
+    if (obj1[objectKey1[i]] == obj2[objectKey2[i]]) {
+      console.log("Key value pair for (" + objectKey1[i] + ") is " + obj1[objectKey1[i]] + " & " + obj2[objectKey2[i]] + ".  We have a match.");
+      return true;
+    }
+  }
+  if (!match) {
+    console.log("There's no matching key-value pair in the 2 objects provided.");
+  }
+  return match;
+}
+
+//test code for longest phrase
 var arryString = ["long phrase","longest phrase","longer phrase"];
 console.log (longestPhrase(arryString));
+
+//test code for key value pair match
+var exampleObject1 = {name: "Steven", age: 54};
+var exampleObject2 = {name: "Tamir", age: 55};
+var exampleObject3 = {name: "Jonny", age: 55};
+
+keyValueMatch(exampleObject1, exampleObject2);
+keyValueMatch(exampleObject2, exampleObject3);
